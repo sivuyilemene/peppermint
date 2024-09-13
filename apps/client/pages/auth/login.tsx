c83eaea0893638d9661c9f323a213975874b3ec7
Sivuyile Mene <smene98@gmail.com>
2024-09-13 11:46:00 +0200

    changed logos for the login page

--- apps/client/pages/auth/login.tsx
+++ apps/client/pages/auth/login.tsx
@@ -84,13 +84,13 @@ export default function Login({}) {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        {/* <a target="_blank" href="https://peppermint.sh/">
+        <a target="_blank" href="https://cloudza.io/">
           <img
             className="mx-auto h-36 w-auto"
             src="/login.svg"
-            alt="peppermint.sh logo"
+            alt="cloudza logo"
           />
-        </a> */}
+        </a> 
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Sign in to your account
         </h2>
@@ -222,13 +222,6 @@ export default function Login({}) {
             </div>
           </div>
         )}
-
-        <div className="mt-8 text-center flex flex-col space-y-2">
-          <span className="font-bold">Built with 💚 by Peppermint Labs</span>
-          <a href="https://docs.peppermint.sh/" target="_blank">
-            Documentation
-          </a>
-        </div>
       </div>
     </div>
   );
